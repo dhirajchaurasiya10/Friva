473e8bd4716fb0bc1609ea0735cedcbcd6dc6628
dhirajchaurasiya10 <starkjon224@gmail.com>
2023-12-27 23:50:34 +0545

    added providers

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:friva/providers/game_page_provider.dart';
+import 'package:provider/provider.dart';
 
 class GamePage extends StatelessWidget {
   double? _deviceHeight, _deviceWidth;
   GamePage({super.key});
 
+  GamePageProvider? _pageProvider;
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
-    return _buildUI();
+    return ChangeNotifierProvider(
+      create: (_context) => GamePageProvider(context: context),
+      child: _buildUI(),
+    );
   }
 
   Widget _buildUI() {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: _deviceHeight!*0.05,),
-          child: _gameUI(),
-        ),
-      ),
+    return Builder(
+      builder: (_context) {
+        _pageProvider = _context.watch<GamePageProvider>();
+        return Scaffold(
+          body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: _deviceHeight! * 0.05,
+              ),
+              child: _gameUI(),
+            ),
+          ),
+        );
+      }
     );
   }
 
@@ -29,15 +43,59 @@ class GamePage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         _questionText(),
+        Column(
+          children: [
+            _trueButton(),
+            SizedBox(
+              height: _deviceHeight! * 0.01,
+            ),
+            _falseButton(),
+          ],
+        ),
       ],
     );
   }
 
-  Widget _questionText(){
-    return Text("Test Question 1, Nothing Interesting", style: TextStyle(
-      color: Colors.white,
-      fontSize: 25,
-      fontWeight: FontWeight.w400,
-    ),);
+  Widget _questionText() {
+    return Text(
+      "Test Question 1, Nothing Interesting",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 25,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _trueButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.green,
+      minWidth: _deviceWidth! * 0.8,
+      height: _deviceHeight! * 0.10,
+      child: Text(
+        "True",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+
+  Widget _falseButton() {
+    return MaterialButton(
+      onPressed: () {},
+      color: Colors.red,
+      minWidth: _deviceWidth! * 0.8,
+      height: _deviceHeight! * 0.10,
+      child: Text(
+        "False",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
+    );
   }
 }
